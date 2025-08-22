9b99a05efa3d4c2db864e898299bdfb2ce95f3fc
Gilberto Carvalhal <gilbertocarvalhalmatos@gmail.com>
2025-08-22 12:44:00 -0300

    Domain: Adiciona Entidade Tarefa

--- app/Entities/Tarefa.php
+++ app/Entities/Tarefa.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace App\Entities;
+
+final class Tarefa
+{
+    public int $id;
+    public string $titulo;
+    public ?string $descricao = null;
+    public ?string $categoriaNome = null;
+    public string $criadaEm;
+}
